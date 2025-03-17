15e7d6bc4be4002a2800ed706e45d264798913af
nyanlinhtet02 <nyan.lin.ft24@mail.umy.ac.id>
2025-03-17 06:55:21 -0700

    addding while loop

--- LoopingSwitchCase_0052.cpp
+++ LoopingSwitchCase_0052.cpp
@@ -2,14 +2,28 @@
 
 using namespace std;
 
-int main (){
+int main ()
+{
 
-}
 int n;
     int perulanganWhile = 0;
     int perulanganDo = 0;
     cout << "Perulangan for pencacah naik" << endl;
     for (n = 0; n < 5; n++)
     {
+        cout << "Nilai n = " << n << " Selamat Datang" << endl;
 
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
 
+    cout << "Perulangan while" << endl;
+    srand(time(0));
+
+    n = rand() % 10;
+
+    cout << "Nilai awal n = " << n << endl;
+
+    while (n < 7)
+
+}
