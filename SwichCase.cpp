1369e6eddd7a314c8209bfb4b9bc6dcb8ae56588
nyanlinhtet02 <nyan.lin.ft24@mail.umy.ac.id>
2025-03-17 07:10:59 -0700

    Using switch function

--- SwichCase.cpp
+++ SwichCase.cpp
@@ -11,6 +11,16 @@ float luasLingkaran(float a) {
         int pilihan;
         float panjang, lebar;
     float jarijari;
+    do{
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan)
 
         
 
